8351b0dbc698999ebec813796c98d9f4a6791ba3
Yuri Lemos <yuri.lemos@parafernalia.net.br>
2023-07-23 16:17:12 -0300

    add button

--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 
 export const NotFound = () => {  
-
+const navigate = useNavigate();
   return (
-    <div>Rota inválida</div>
+    <div>Rota inválida
+      <button onClick={()=>{navigate('/')}}>voltar</button>
+    </div>
   );
 };
